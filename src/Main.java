fdb12b58b6d98a570ec54725036b9714fb89f867
user <Seregawinner92@gmail.com>
2024-01-17 23:48:25 +0300

    correction

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Привет Мир!");
         System.out.println("Hello World!");
+
         var dog = 8.0;
         System.out.println(dog);
         var cat = 3.6;
