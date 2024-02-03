ebbcb11d3b281c3a26938fd0382c302c6beded10
user <Seregawinner92@gmail.com>
2024-02-03 15:07:44 +0300

    conditional operator lesson two homework changing tasks  two

--- src/Main.java
+++ src/Main.java
@@ -6,21 +6,23 @@ public class Main {
         int clientOS = 1;
         int clientiOS = 0;
         int clientAndroid = 1;
-        if  (clientiOS == clientOS){
+        if (clientiOS == clientOS) {
             System.out.println("Установите версию приложения для iOS по ссылке");
-        } else if (clientAndroid == clientOS){
+        } else if (clientAndroid == clientOS) {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
 
         System.out.println("Задание 2");
         int clientDeviceYear = 2016;
-        if (clientOS == 0 && clientDeviceYear < 2015) {
+        int frontier = 2015;
+        if (clientOS == clientiOS && clientDeviceYear < frontier) {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-        } if (clientOS == 0 && clientDeviceYear >= 2015) {
+        }
+        if (clientOS == clientiOS && clientDeviceYear >= frontier) {
             System.out.println("Установите версию приложения для iOS по ссылке");
-        } else if (clientOS == 1 && clientDeviceYear < 2015) {
+        } else if (clientOS == clientAndroid && clientDeviceYear < frontier) {
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
-        } else if (clientOS == 1 && clientDeviceYear >= 2015) {
+        } else if (clientOS == clientAndroid && clientDeviceYear >= frontier) {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
 
@@ -30,7 +32,7 @@ public class Main {
         int b = year % 100;
         int c = year % 4;
         if (year > 1584 && c == 0 && b != 0 || a == 0) {
-            System.out.println( year + " год является високосным!");
+            System.out.println(year + " год является високосным!");
         } else {
             System.out.println(year + " год не являеться високосным");
         }
@@ -38,22 +40,25 @@ public class Main {
         System.out.println("Задание 4");
         int deliveriDistance = 105;
         int intervalA = 20;
-        int intervalB = intervalA+40;
-        int intervalC = intervalB+40;
+        int intervalB = intervalA + 40;
+        int intervalC = intervalB + 40;
         int dayA = 1;
         int dayB = 2;
         int dayC = 3;
         if (deliveriDistance <= intervalA) {
-            System.out.println("Потребуется дней: " + dayA);}
+            System.out.println("Потребуется дней: " + dayA);
+        }
         if (deliveriDistance > intervalA && deliveriDistance <= intervalB) {
             System.out.println("Потребуется дней: " + dayB);
-        } else if (deliveriDistance > intervalB && deliveriDistance < intervalC){
+        } else if (deliveriDistance > intervalB && deliveriDistance < intervalC) {
             System.out.println("Потребуется дней: " + dayC);
-        } else { System.out.println("Доставка не осуществляется");}
+        } else {
+            System.out.println("Доставка не осуществляется");
+        }
 
         System.out.println("Задание 5");
         int monthNumber = 12;
-        switch (monthNumber){
+        switch (monthNumber) {
             case 1:
             case 2:
             case 12:
